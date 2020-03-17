146c491f546d0a783506565d4c626b24a03a547d
Danilo Mourelle <danilomourelle@outlook.com>
2020-03-16 21:01:15 -0300

    Preparação do ambiente, instalação das dependencias, e componente gera operação

--- src/Components/Planilha.js
+++ src/Components/Planilha.js
@@ -1,54 +1,66 @@
+function valorTotal (valorUnitario, quantidade) {
+  return valorUnitario * quantidade
+}
+function liquidacao (valorTotal){
+  return valorTotal * 0.000275
+}
+function negociacao (valorTotal){
+  return valorTotal * 0.00003248
+}
+function emolumentos (liquidacao, negociacao){
+  return liquidacao + negociacao
+}
+function totalOperacao (valorTotal, emolumentos, tipo){
+  if (tipo==="compra"){
+   return  valorTotal + emolumentos
+  } else if (tipo === "venda"){
+    return valorTotal - emolumentos
+  }
+}
+function precoMedio (totalOperacao, quantidade){
+  return totalOperacao / quantidade
+}
+
 class Operacao {
-  constructor(ticker, tipo, valorUnitario, quantidade) {
+  constructor(ticker, tipo, valorUnitario, quantidade, valorTotal, liquidacao, negociacao, emolumentos, totalOperacao, precoMedio) {
     this.ticker = ticker
     this.tipo = tipo
     this.valorUnitario = valorUnitario
     this.quantidade = quantidade
-    this.valorTotal = valorUnitario * quantidade
-    this.liquidacao = this.valorTotal * 0.000275
-    this.negociacao = this.valorTotal * 0.00003248 // usar ternário
-    this.emolumentos = this.liquidacao + this.negociacao
-    this.totalOperacao = this.emolumentos + this.valorTotal
-    this.precoMedio = this.totalOperacao / this.quantidade
-  }
-  set calculosEmolumentos (valorTotal) {
-    this.liquidacao = 
-    }
-
-}
-
-let bla
-function teste (novObjeto) {
-  novObjeto = new Operacao('BPAN', 'C', 9.98, 100)
-  novObjeto.calculosEmolumentos(novObjeto.valorTotal)
-}
-
-teste(bla)
-console.log(bla)
-
-/* class Person {
-  constructor(name) {
-    this._name = name;
-  }
-  
-  get bla() {
-    return this._name.toUpperCase();
-  }
-
-  set name(name) {
-    this._name = name + name; // validation could be checked here such as only allowing non numerical values
-  }
-
-  walk() {
-    console.log(this._name + ' is walking.');
+    this.valorTotal = valorTotal
+    this.liquidacao = liquidacao
+    this.negociacao = negociacao
+    this.emolumentos = emolumentos
+    this.totalOperacao = totalOperacao
+    this.precoMedio = precoMedio
   }
 }
 
-let bob = new Person('Bob');
-console.log(bob.bla); // Outputs 'BOB'
-console.log(bob._name); // Outputs 'BOB'
+export function crianOperacao (ticker, tipo, valorUnitario, quantidade){
+  let novoValorTotal = valorTotal(valorUnitario, quantidade)
+  let novoLiquidacao = liquidacao(novoValorTotal)
+  let novoNegociacao = negociacao(novoValorTotal)
+  let novoEmolumentos = emolumentos(novoLiquidacao, novoNegociacao)
+  let novoTtlOperacao = totalOperacao(novoValorTotal, novoEmolumentos, tipo)
+  let novoPrecoMedio = precoMedio(novoTtlOperacao, quantidade)
 
-console.log(bob.bla); // Outputs 'BOB'
-console.log(bob._name); // Outputs 'BOB'
- // Outputs 'BOB'
- */
+  let novaOperacao = new Operacao (
+    ticker, 
+    tipo, 
+    valorUnitario, 
+    quantidade, 
+    novoValorTotal, 
+    novoLiquidacao, 
+    novoNegociacao, 
+    novoEmolumentos, 
+    novoTtlOperacao, 
+    novoPrecoMedio
+  )
+
+  return novaOperacao
+}
+
+
+
+
+
